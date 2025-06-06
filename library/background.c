5089ccb0496df5c30d2ab7f38358df15169b30e0
ali3@caltech.edu <ali3@caltech.edu>
2025-06-06 00:05:06 +0000

    update vel

--- library/background.c
+++ library/background.c
@@ -35,7 +35,7 @@ void background_init(state_t *state) {
 
 // increase speed of player + backgrounds
 void update_bg_velocity(state_t *state) {
-  vector_t add = (vector_t) {.x = 0.01, .y = 0};
+  vector_t add = (vector_t){.x = 0.01, .y = 0};
   state->bg.bg_1_sky_vel = vec_add(add, state->bg.bg_1_sky_vel);
   state->bg.bg_2_tree_vel = vec_add(add, state->bg.bg_2_tree_vel);
   state->bg.bg_3_building_vel = vec_add(add, state->bg.bg_3_building_vel);
