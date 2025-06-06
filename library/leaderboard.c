66aee731b3c465e0d22aafd61a6a776b1fb3de01
agurumoo@caltech.edu <agurumoo@caltech.edu>
2025-06-06 20:35:57 +0000

    Formatting

--- library/leaderboard.c
+++ library/leaderboard.c
@@ -12,10 +12,13 @@
 #include "obstacle.h"
 
 body_t *render_scoreboard(state_t *state) {
-  //NOTE: TTF_Init inside emscripten main
+  // NOTE: TTF_Init inside emscripten main
   vector_t center = (vector_t){MAX.x - LEADERBOARD_SIZE.x / 2,
-    MAX.y - LEADERBOARD_SIZE.y / 2};
-  
-  body_t *scoreboard_bg_rectangle = make_rectangle_body(LEADERBOARD_SIZE.x, LEADERBOARD_SIZE.y, center, LEADERBOARD_COLOR);
-  body_t *text_rectangle = sdl_get_rect(MAX.x - LEADERBOARD_SIZE.x*0.75, MAX.y - LEADERBOARD_SIZE.y*0.75, LEADERBOARD_SIZE.x*0.5, LEADERBOARD_SIZE.y*0.5);
+                               MAX.y - LEADERBOARD_SIZE.y / 2};
+
+  body_t *scoreboard_bg_rectangle = make_rectangle_body(
+      LEADERBOARD_SIZE.x, LEADERBOARD_SIZE.y, center, LEADERBOARD_COLOR);
+  body_t *text_rectangle = sdl_get_rect(
+      MAX.x - LEADERBOARD_SIZE.x * 0.75, MAX.y - LEADERBOARD_SIZE.y * 0.75,
+      LEADERBOARD_SIZE.x * 0.5, LEADERBOARD_SIZE.y * 0.5);
 }
