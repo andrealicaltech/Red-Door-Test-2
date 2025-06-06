d989efcc6c8d4ef4fb840e489ca1ade458cd448f
agurumoo@caltech.edu <agurumoo@caltech.edu>
2025-06-06 09:24:15 +0000

    Formatting

--- library/leaderboard.c
+++ library/leaderboard.c
@@ -12,11 +12,10 @@
 #include "obstacle.h"
 
 body_t *render_scoreboard(state_t *state) {
-  body_t* scoreboard_bg = make_scoreboard_background();
-
+  body_t *scoreboard_bg = make_scoreboard_background();
 }
 
-body_t* make_scoreboard_background(){
+body_t *make_scoreboard_background() {
   list_t *c = list_init(4, free);
   vector_t *v1 = malloc(sizeof(vector_t));
   *v1 = (vector_t){MAX.x, MAX.y};
@@ -41,6 +40,3 @@ body_t* make_scoreboard_background(){
   body_set_centroid(scoreboard, center);
   return scoreboard;
 }
-
-
-
