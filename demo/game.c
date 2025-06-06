f7c6f636fe52acdf59c7951df12ca0da6b023fd9
ali3@caltech.edu <ali3@caltech.edu>
2025-06-06 20:27:59 +0000

    c

--- demo/game.c
+++ demo/game.c
@@ -22,7 +22,8 @@
 /*
 MARK: Player control and kinematics
 */
-body_t *make_rectangle_body(double width, double height, vector_t center, bool player) {
+body_t *make_rectangle_body(double width, double height, vector_t center,
+                            bool player) {
   list_t *rect = list_init(4, free);
 
   vector_t *vec_1 = malloc(sizeof(vector_t));
@@ -43,7 +44,7 @@ body_t *make_rectangle_body(double width, double height, vector_t center, bool p
 
   if (player) {
     body_t *rectangle =
-      body_init_with_info(rect, 1, SPRITE_COLOR, (void *)PLAYER_INFO, NULL);
+        body_init_with_info(rect, 1, SPRITE_COLOR, (void *)PLAYER_INFO, NULL);
     return rectangle;
   } else {
     body_t *obstacle = body_init(rect, 1, SPRITE_COLOR);
@@ -96,17 +97,20 @@ state_t *emscripten_init() {
   state->player_motion = REGULAR;
 
   // Needs to be the first one
-  body_t *player = make_rectangle_body(PLAYER_DIMS.x, PLAYER_DIMS.y, VEC_ZERO, true);
+  body_t *player =
+      make_rectangle_body(PLAYER_DIMS.x, PLAYER_DIMS.y, VEC_ZERO, true);
   body_set_centroid(player, PLAYER_CENTER_POS);
   state->player = player;
   scene_add_body(state->scene, player);
 
   // TODO: Initialize all 3 backgrounds
   background_init(state);
-  body_t *sky = make_rectangle_body(state->bg.sky_pos.x, state->bg.sky_pos.y, MIN, false);
-  body_t *tree = make_rectangle_body(state->bg.tree_pos.x, state->bg.tree_pos.y, MIN, false);
-  body_t *building =
-      make_rectangle_body(state->bg.building_pos.x, state->bg.building_pos.x, MIN, false);
+  body_t *sky =
+      make_rectangle_body(state->bg.sky_pos.x, state->bg.sky_pos.y, MIN, false);
+  body_t *tree = make_rectangle_body(state->bg.tree_pos.x, state->bg.tree_pos.y,
+                                     MIN, false);
+  body_t *building = make_rectangle_body(state->bg.building_pos.x,
+                                         state->bg.building_pos.x, MIN, false);
   asset_make_image_with_body(SKY_PATH, sky);
   asset_make_image_with_body(TREE_PATH, tree);
   asset_make_image_with_body(BUILDING_PATH, building);
