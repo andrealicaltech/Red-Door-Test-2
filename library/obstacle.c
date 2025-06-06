8167642f28244dcffdfafc916575eb885d60b69a
ali3@caltech.edu <ali3@caltech.edu>
2025-06-06 07:58:08 +0000

    stack error

--- library/obstacle.c
+++ library/obstacle.c
@@ -180,10 +180,10 @@ void update_obstacles(state_t *state) {
     // Extremely small value
     x = -1.0;
   } else {
-    // Use 1 + n_q - 1 for clarity: first body is player, so add 1. Subtract 1
+    // Use 1 + n_q - 1 for clarity: first body is player, next 3 are backgrounds. Subtract 1
     // for zero-indexing
     body_t *last_obstacle =
-        scene_get_body(state->scene, (1 + state->n_queued_obstacles) - 1);
+        scene_get_body(state->scene, (4 + state->n_queued_obstacles) - 1);
     double next_obst_x_val = next_obst_x(state, last_obstacle);
     x = next_obst_x_val;
     // printf("new x=%f\n", x);
