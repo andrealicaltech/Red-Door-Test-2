7c14f9104b08882f7929d1b9de3d879655d0756c
agurumoo@caltech.edu <agurumoo@caltech.edu>
2025-06-06 20:47:22 +0000

    Formatting

--- include/leaderboard.h
+++ include/leaderboard.h
@@ -6,7 +6,8 @@
 /*
 Makes a scoreboard showing the player's score as they progress through a level.
 */
-body_t *make_scoreboard(double width, double height, vector_t center, color_t color);
+body_t *make_scoreboard(double width, double height, vector_t center,
+                        color_t color);
 
 body_t *render_scoreboard(state_t *state);
 
--- library/leaderboard.c
+++ library/leaderboard.c
@@ -11,27 +11,28 @@
 #include "kinematics.h"
 #include "obstacle.h"
 
-body_t *make_scoreboard(double width, double height, vector_t center, color_t color) {
-    list_t *rect = list_init(4, free);
-  
-    vector_t *vec_1 = malloc(sizeof(vector_t));
-    *vec_1 = (vector_t){0, 0};
-    list_add(rect, vec_1);
-  
-    vector_t *vec_2 = malloc(sizeof(vector_t));
-    *vec_2 = (vector_t){width, 0};
-    list_add(rect, vec_2);
-  
-    vector_t *vec_3 = malloc(sizeof(vector_t));
-    *vec_3 = (vector_t){width, height};
-    list_add(rect, vec_3);
-  
-    vector_t *vec_4 = malloc(sizeof(vector_t));
-    *vec_4 = (vector_t){0, height};
-    list_add(rect, vec_4);
-    body_t *leaderboard = body_init(rect, 1, color);
-    return leaderboard;
-  }
+body_t *make_scoreboard(double width, double height, vector_t center,
+                        color_t color) {
+  list_t *rect = list_init(4, free);
+
+  vector_t *vec_1 = malloc(sizeof(vector_t));
+  *vec_1 = (vector_t){0, 0};
+  list_add(rect, vec_1);
+
+  vector_t *vec_2 = malloc(sizeof(vector_t));
+  *vec_2 = (vector_t){width, 0};
+  list_add(rect, vec_2);
+
+  vector_t *vec_3 = malloc(sizeof(vector_t));
+  *vec_3 = (vector_t){width, height};
+  list_add(rect, vec_3);
+
+  vector_t *vec_4 = malloc(sizeof(vector_t));
+  *vec_4 = (vector_t){0, height};
+  list_add(rect, vec_4);
+  body_t *leaderboard = body_init(rect, 1, color);
+  return leaderboard;
+}
 
 body_t *render_scoreboard(state_t *state) {
   // NOTE: TTF_Init inside emscripten main
