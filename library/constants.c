feed4a3cd73e48744ce89071934f1288cbbf61fd
ali3@caltech.edu <ali3@caltech.edu>
2025-06-08 22:42:13 +0000

    card

--- library/constants.c
+++ library/constants.c
@@ -61,6 +61,7 @@ const size_t BODY_ASSETS = 1;
 const char *START_SCREEN_PATH = "assets/Start.png";
 const char *GAME_OVER_PATH = "assets/Game_Over.png";
 const char *SHOP_PATH = "assets/Shop.png";
+const char *CARD_PATH = "assets/Card.png";
 const char *PLAYER_SPRITE_AMUDHAN_PATH = "assets/Amudhan.png";
 const char *PLAYER_SPRITE_ANDREA_PATH = "assets/Andrea.png";
 const char *PLAYER_SPRITE_ARJUN_PATH = "assets/Arjun.png";
