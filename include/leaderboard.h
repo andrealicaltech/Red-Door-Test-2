d3f0647d461b904207663f4f082fbeb4abbfe6e5
agurumoo@caltech.edu <agurumoo@caltech.edu>
2025-06-08 02:29:25 +0000

    Formatting

--- include/leaderboard.h
+++ include/leaderboard.h
@@ -7,10 +7,10 @@
 Makes a scoreboard showing the player's score as they progress through a level.
 */
 void make_scoreboard(double width, double height, vector_t center,
-                        color_t color);
+                     color_t color);
 
 void render_scoreboard(state_t *state);
 
-void update_text(state_t *state) 
+void update_text(state_t *state)
 
 #endif
